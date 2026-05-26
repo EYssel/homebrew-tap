8ef6c026ca61dc69a121d7d47c7c13040937a7c1
Estian Yssel <estianyssel@gmail.com>
2026-05-26 11:03:52 +0200

    fix: weekly-planner URL filename and caveats

--- Casks/weekly-planner.rb
+++ Casks/weekly-planner.rb
@@ -2,7 +2,7 @@ cask "weekly-planner" do
   version "1.18.0"
   sha256 "35ff6f06995db7a94ff08837109096d414987c63c8dbcf389f872ae0fe9f8bc0"
 
-  url "https://github.com/EYssel/planner-app/releases/download/v#{version}/Weekly%20Planner-#{version}-arm64.dmg"
+  url "https://github.com/EYssel/planner-app/releases/download/v#{version}/Weekly-Planner-#{version}-arm64.dmg"
   name "Weekly Planner"
   desc "A simple weekly planning desktop app"
   homepage "https://github.com/EYssel/planner-app"
@@ -12,8 +12,6 @@ cask "weekly-planner" do
   caveats <<~EOS
     Because this app is self-signed/unsigned, you may need to clear the quarantine attribute after installation:
       xattr -cr "/Applications/Weekly Planner.app"
-    Alternatively, you can install it using:
-      brew install --cask --no-quarantine weekly-planner
   EOS
 
   zap trash: [
